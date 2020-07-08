ca15140090c1a7930854de71f0c175fe04d2724d
Nicky Pochinkov <pochinkn@tcd.ie>
2020-07-08 20:50:35 +0100

    Made file serving optional through node express

--- server.js
+++ server.js
@@ -9,16 +9,6 @@ require('dotenv').config({ path: path.resolve(__dirname, './.env') })
 const imagesDir = process.env.IMAGE_PATH || path.join(__dirname, "/images/") 
 const TIME_INTERVAL = 12 * 3600 * 1000 /* milliseconds = every 12 hours*/
 
-//initialise node express app
-const app = express();
-
-//Add a logger of requests
-const logger = (req, res, next) => {
-  console.log(`${new Date().toUTCString()} - ${req.method} ${req.path} - ${req.ip}`);
-  next();
-}
-app.use(logger)
-
 //initialise database with files
 updateImagesDatabase(imagesDir)
 
@@ -26,12 +16,27 @@ setInterval(() =>
   updateImagesDatabase(imagesDir)
 , TIME_INTERVAL)
 
-//serve the images directory
-app.use('/images', express.static(imagesDir));
+// make serving files optional
+if ( !process.env.DO_NOT_SERVE ) {
 
-//start the server on PORT
-//would also be better to just serve the files using nginx/apache 
-const PORT = process.env.PORT || 80
-app.listen(PORT, function () {
-    console.log(`Listening on http://localhost:${PORT}/`);
-}); 
+  //initialise node express app
+  const app = express();
+
+  //Add a logger of requests
+  const logger = (req, res, next) => {
+    console.log(`${new Date().toUTCString()} - ${req.method} ${req.path} - ${req.ip}`);
+    next();
+  }
+  app.use(logger)
+
+  //serve the images directory
+  app.use('/images', express.static(imagesDir));
+
+  //start the server on PORT
+  //would also be better to just serve the files using nginx/apache 
+  const PORT = process.env.PORT || 80
+  app.listen(PORT, function () {
+      console.log(`Listening on http://localhost:${PORT}/`);
+  }); 
+
+}
