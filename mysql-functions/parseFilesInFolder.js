1413d0b859e517b7d38b5e399fa6a04f7c4442b0
Nicky Pochinkov <pochinkn@tcd.ie>
2020-08-25 15:22:33 +0100

    made it so that it checks that a file is a png

--- mysql-functions/parseFilesInFolder.js
+++ mysql-functions/parseFilesInFolder.js
@@ -5,7 +5,7 @@ function parseFromFileName(file) {
   //we need to convert this to NAME and YYYY-MM-DD HH:mm:SS.sss
   //Example: FF_IE0001_20200403_051538_014_0857856.png
   const dateRegex = /(\d{4})(\d{2})(\d{2})_(\d{2})(\d{2})(\d{2})_(\d{3})/
-  const testRegex = /(.*)_(\d{8})_(\d{6})_(\d{3})/
+  const testRegex = /(.*)_(\d{8})_(\d{6})_(\d{3})(.*)(\.png)/
 
   if ( testRegex.test(file) ) {
     const rawDateString = file.match(dateRegex)[0]
