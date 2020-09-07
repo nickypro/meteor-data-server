9af6444d4f9f515d857e3126fdd2b07dca12b921
Nicky Pochinkov <pochinkn@tcd.ie>
2020-09-07 13:24:08 +0100

    removed database update from readFTPdetectinfo

--- mysql-functions/readFTPdetectinfoFile.js
+++ mysql-functions/readFTPdetectinfoFile.js
@@ -1,7 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-const Op = require('sequelize').Op;
-const Image = require('./sequelize');
 
 function readFTPdetectinfoFile(file) {
   const detectinfoTest = /FTPdetectinfo[a-zA-Z0-9_.-]*\.txt/
@@ -15,25 +13,14 @@ function readFTPdetectinfoFile(file) {
       var data = fs.readFileSync(file, 'utf8');
       const fitsList = data.match(fitsImageTest)
       const pngList = fitsList.map(item => item.replace(".fits", ".png")) 
-      console.log(pngList)
+      
+      return pngList
 
-      Image.update({
-        stars: 1,
-      },{
-        where:{
-          stars: {
-            [Op.lt]: 1
-          },
-          fileName: {
-            [Op.in]: pngList
-          }
-        }
-      })
-    
     }
   
   } catch(err) {
     console.log('Error:', err.message);
+    return []
   }
 }
 
