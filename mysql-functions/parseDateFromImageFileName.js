0a909dde56922f5108e6c1e832f0185f3666ab58
Nicky Pochinkov <pochinkn@tcd.ie>
2020-09-07 13:07:35 +0100

    moced parseDateFromFileName to separate file

---
+++ mysql-functions/parseDateFromImageFileName.js
@@ -0,0 +1,19 @@
+function parseDateFromImageFileName(file) {
+  //we have files of the format NAME_YYYYMMDD_HHmmSS_sss_NUMBERS.png
+  //we need to convert this to NAME and YYYY-MM-DD HH:mm:SS.sss
+  //Example: FF_IE0001_20200403_051538_014_0857856.png
+  const dateRegex = /(\d{4})(\d{2})(\d{2})_(\d{2})(\d{2})(\d{2})_(\d{3})/
+  const testRegex = /(.*)_(\d{8})_(\d{6})_(\d{3})(.*)(\.png)/
+
+  if ( testRegex.test(file) ) {
+    const rawDateString = file.match(dateRegex)[0]
+    const dateString = rawDateString.replace(dateRegex, "$1-$2-$3 $4:$5:$6.$7 GMT")
+    const date = new Date(dateString)
+    
+    const camera = file.match(testRegex)[1]
+    
+    return {date, camera}
+  }
+
+  return {}
+}
--- mysql-functions/parseFilesInFolder.js
+++ mysql-functions/parseFilesInFolder.js
@@ -1,25 +1,5 @@
 const fs = require('fs');
-
-function parseFromFileName(file) {
-  //we have files of the format NAME_YYYYMMDD_HHmmSS_sss_NUMBERS.png
-  //we need to convert this to NAME and YYYY-MM-DD HH:mm:SS.sss
-  //Example: FF_IE0001_20200403_051538_014_0857856.png
-  const dateRegex = /(\d{4})(\d{2})(\d{2})_(\d{2})(\d{2})(\d{2})_(\d{3})/
-  const testRegex = /(.*)_(\d{8})_(\d{6})_(\d{3})(.*)(\.png)/
-
-  if ( testRegex.test(file) ) {
-    const rawDateString = file.match(dateRegex)[0]
-    const dateString = rawDateString.replace(dateRegex, "$1-$2-$3 $4:$5:$6.$7 GMT")
-    const date = new Date(dateString)
-    
-    const camera = file.match(testRegex)[1]
-    
-    return {date, camera}
-  }
-
-  return {}
-}
-
+const ParseDateFromImageFileName = require('./parseDateFromImageFileName')
 //parses information from each file in a folder
 function parseFilesInFolder(folder, folderPath) {
 
@@ -32,7 +12,7 @@ function parseFilesInFolder(folder, folderPath) {
   files.forEach(file => {
       
     //get time of image
-    const {date, camera} = parseFromFileName(file)
+    const {date, camera} = parseDateFromImageFileName(file)
     if(!date) {
       return;
     }
